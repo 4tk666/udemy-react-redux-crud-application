a52f457ef343176a7927a59f2d2bb7baba5789b7
4tk666 <vhfxhy5356@yahoo.co.jp>
2020-03-14 17:58:47 +0900

    todoアプリの作成

--- src/App.js
+++ src/App.js
@@ -1,11 +1,65 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div>
-      <h1>Hello,world!</h1>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      value: '',
+      items: []
+    };
+  }
+
+  handleChange = (e) => {
+    const value = e.target.value;
+    this.setState({
+      value: value
+    });
+  }
+
+  
+  handleSubmit = () => {
+    const newItem = {
+      value: this.state.value
+    }
+    
+    if(newItem.value !== ''){
+      this.setState(state => ({
+        items: state.items.concat(newItem),
+        value: ''
+      }));
+    }
+  }
+
+  handleRemove = (i) => {
+    this.state.items.splice(i,1)
+    this.setState({items: this.state.items})
+  }
+
+
+  render(){
+    return(
+      <React.Fragment>
+        <input type="text" name="value" placeholder="タスクを入力してください" value={this.state.value} onChange={this.handleChange}/>
+        <button onClick={this.handleSubmit}>追加する</button>
+        <TodoList items={this.state.items} handleRemove={this.handleRemove}/>
+      </React.Fragment>
+    )
+  }
 }
+
+class TodoList extends React.Component {
+  render(){
+    return(
+      <React.Fragment>
+        <ul>
+          {this.props.items.map((item,i) =>(
+            <li key={i}>{item.value}<button onClick={this.props.handleRemove}>削除する</button></li>
+          ))}
+        </ul>
+      </React.Fragment>
+    )
+  }
+}
+
 
 export default App;
